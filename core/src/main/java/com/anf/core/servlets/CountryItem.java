2a2dd68aa7c12ad7d03c5e738290d092db98a549
sansai2011 <san.sai2011@gmail.com>
2022-09-22 20:04:57 +0530

    feature(countryList) - populate List in dialog & display in frontend

---
+++ core/src/main/java/com/anf/core/servlets/CountryItem.java
@@ -0,0 +1,12 @@
+package com.anf.core.servlets;
+
+public class CountryItem {
+    String key;
+    String value;
+
+    // constructor
+    public CountryItem(String inKey, String inValue){
+        this.key = inKey;
+        this.value = inValue;
+    }
+}
